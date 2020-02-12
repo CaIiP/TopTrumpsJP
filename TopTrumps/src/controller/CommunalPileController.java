19aecf7095baf15acd2aa9a3d4cdab2b5dacb2ab
CaIum Paterson <56022431+CaIiP@users.noreply.github.com>
2020-02-12 13:45:33 +0000

    Revert "asks to play again or show statistics at end of game."
    This reverts commit 437498c7bfa050dea682e45f91188f319076b54a.

--- TopTrumps/src/controller/CommunalPileController.java
+++ TopTrumps/src/controller/CommunalPileController.java
@@ -4,11 +4,10 @@ import model.Card;
 import model.CommunalPile;
 
 public class CommunalPileController {
-	//function used to obtain a letter from the communal pile
 	public void giveCard(Card newCard,CommunalPile cp) {
-		Card[] newHand = new Card[cp.getCards().length + 1];//a array letter object is created with a communal pile letter
-		System.arraycopy(cp.getCards(), 0, newHand, 0, cp.getCards().length);//all the letters to this new object of the communal pile are copied
-		newHand[cp.getCards().length] = newCard;//new cards are assigned
-		cp.setCards(newHand);//and then we assign this new arrangement to the communal pile
+		Card[] newHand = new Card[cp.getCards().length + 1];
+		System.arraycopy(cp.getCards(), 0, newHand, 0, cp.getCards().length);
+		newHand[cp.getCards().length] = newCard;
+		cp.setCards(newHand);
 	}
 }
